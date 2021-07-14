4fcd446477ebf6d8437ce7151095db7244c6d15c
Parola Marco <mark.prl.97@gmail.com>
2021-07-14 16:13:09 +0200

    "build.sh" now reads and uses the env var "installpath" (useful for building packages)

--- build.sh
+++ build.sh
@@ -5,11 +5,16 @@ set -e # Terminate the script when an error occurs
 config="${1:-"Debug"}"
 generator="${generator:-"Ninja"}"
 
-srcpath="$(realpath .)"
-dstpath=build-"$config"
+srcpath="${srcpath:-"$(realpath .)"}"
+dstpath="${dstpath:-build-"$config"}"
+
+if [[ -v installpath ]]
+then install=("-DCMAKE_INSTALL_PREFIX=$installpath")
+else install=()
+fi
 
 mkdir -p "$dstpath"
 cd "$dstpath"
 
-cmake -DCMAKE_BUILD_TYPE="$config" "$srcpath" -G "$generator"
+cmake -DCMAKE_BUILD_TYPE="$config" "${install[@]}" "$srcpath" -G "$generator"
 cmake --build . --config "$config"
